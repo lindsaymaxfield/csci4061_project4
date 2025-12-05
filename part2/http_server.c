6536862b3020fc1c720a9f2e09794655b048d62c
Devpacito <32854595+Dev9awesome@users.noreply.github.com>
2025-12-04 20:08:24 -0600

    Started multi-threaded implementation

--- part2/http_server.c
+++ part2/http_server.c
@@ -25,6 +25,33 @@ void handle_sigint(int signo) {
     keep_going = 0;
 }
 
+// function for worker threads to perform
+// reads request and writes response
+void *worker_thread(void *arg) {
+    connection_queue_t *queue = arg;
+    char resource_name[BUFSIZE];
+    char resource_path[BUFSIZE];
+
+    while (1) {
+        int fd = connection_queue_dequeue(queue);
+        if (fd == -1 && queue->shutdown) {
+            // exit if file descriptor is invalid and queue has shutdown
+            break;
+        }
+
+        read_http_request(fd, resource_name);
+
+        // TODO: COPIED FROM PART 1: DOUBLE CHECK
+        strcpy(resource_path, serve_dir);
+        strcat(resource_path, resource_name);
+
+        write_http_response(fd, resource_path);
+
+        close(fd);    // TODO: error check
+    }
+
+    return NULL;
+}
 
 int main(int argc, char **argv) {
     // First argument is directory to serve, second is port
@@ -33,9 +60,65 @@ int main(int argc, char **argv) {
         return 1;
     }
     // Uncomment the lines below to use these definitions:
-    // serve_dir = argv[1];
-    // const char *port = argv[2];
+    serve_dir = argv[1];
+    const char *port = argv[2];
 
     // TODO Implement the rest of this function
+
+    sigset_t main_mask;      // set that stores current signal mask
+    sigset_t worker_mask;    // set used to block signals to worker threads
+    sigfillset(&worker_mask);
+    sigprocmask(SIG_BLOCK, &worker_mask, &main_mask);    // block all signals to workers
+
+    // Create worker threads
+    connection_queue_t queue;
+    pthread_t threads[N_THREADS];
+    for (int i = 0; i < N_THREADS; i++) {
+        pthread_create(&threads[i], NULL, worker_thread, &queue);
+    }
+
+    // Revert to mask before creating threads
+    sigprocmask(SIG_SETMASK, &main_mask, NULL);
+
+    // Catch SIGINT so we can clean up properly
+    struct sigaction sigact;
+    sigact.sa_handler = handle_sigint;
+    sigfillset(&sigact.sa_mask);
+    sigact.sa_flags = 0;    // No SA_RESTART
+    if (sigaction(SIGINT, &sigact, NULL) == -1) {
+        perror("sigaction");
+        return 1;
+    }
+
+    // TODO: TAKEN FROM PART 1; DOUBLE CHECK
+
+    // Setup TCP Server
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;    // TCP
+    hints.ai_flags = AI_PASSIVE;        // Will be acting as a server
+
+    struct addrinfo *server;
+    getaddrinfo(NULL, &port, &hints, &server);    // TODO: error check
+    int sock_fd =
+        socket(server->ai_family, server->ai_socktype, server->ai_protocol);    // TODO: error check
+    bind(sock_fd, server->ai_addr, server->ai_protocol);                        // TODO: error check
+    listen(sock_fd, LISTEN_QUEUE_LEN);                                          // TODO: error check
+
+    // Main thread loop
+    while (keep_going) {
+        int client_fd = accept(sock_fd, NULL, NULL);
+        connection_queue_enqueue(&queue, client_fd);
+    }
+
+    // Once SIGINT has been sent
+    connection_queue_shutdown(&queue);
+    for (int i = 0; i < N_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    connection_queue_free(&queue);
+    close(sock_fd);
+
     return 0;
 }
